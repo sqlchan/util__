838bce8818f63925971e14d5d64eaaed1d6f5ead
sqlchan <1062511908@qq.com>
2018-12-22 17:11:36 +0800

    apach commons字符串

--- string_apache/src/main/java/apache/a.java
+++ string_apache/src/main/java/apache/a.java
@@ -1,4 +1,12 @@
 package apache;
 
 public class a {
+    asdfasdfasdf；
+            * @author <a href="mailto:hps@intermeta.de">Henning P. Schmiedehausen</a>
+            * @author Arun Mammen Thomas
+ * @author Gary Gregory
+ * @author Phil Steitz
+ * @author Al Chou
+ * @author Michael Davey
+ * @author Reuben Sivan
 }
